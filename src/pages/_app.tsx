11b304608279678d6c274108f5b984a9d4d1963d
Donald Silveira <donaldsilveira@gmail.com>
2023-01-07 21:30:16 -0300

    feat: default SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from '@next/font/google'
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.config'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <style jsx global>
         {`
           :root {
