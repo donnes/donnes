c7000ca8444b0633eb7cee47591bcaf4d544fd38
Donald Silveira <donaldsilveira@gmail.com>
2024-06-02 17:49:39 -0300

    Init v2 with Astro

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "pt-br"],
+  },
+  integrations: [tailwind()],
+});
