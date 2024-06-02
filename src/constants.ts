d76e3d0537d95d6dfcb392e9dea4182b8cab4d22
Donald Silveira <donaldsilveira@gmail.com>
2024-06-02 17:50:12 -0300

    Home design

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const BASED_CITY_LINK =
+  "https://www.google.com/maps/@-30.0785166,-51.2481911,3a,80.4y,356.92h,74.03t/data=!3m7!1e1!3m5!1sAF1QipPWf24TQoLGOy0aB54NHGg60BpeJoBb1kEvLPaU!2e10!3e11!7i8192!8i4096?coh=205410&entry=ttu";
