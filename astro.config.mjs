5c4c28e50b688249b177ef43fbe5c0355d7979a7
Donald Silveira <donaldsilveira@gmail.com>
2024-06-03 18:28:22 -0300

    Updated constants

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://donnes.dev",
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "pt-br"],
+    locales: ["en", "pt-br"]
   },
-  integrations: [tailwind(), mdx(), react()],
+  integrations: [tailwind(), mdx(), react(), sitemap()]
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
-export const BASED_CITY_LINK =
-  "https://www.google.com/maps/@-30.0785166,-51.2481911,3a,80.4y,356.92h,74.03t/data=!3m7!1e1!3m5!1sAF1QipPWf24TQoLGOy0aB54NHGg60BpeJoBb1kEvLPaU!2e10!3e11!7i8192!8i4096?coh=205410&entry=ttu";
-
-export const CV_LINK = "https://read.cv/donnes";
+export const SITE = {
+  TITLE: "Donald Silveira",
+  DESCRIPTION:
+    "I'm a Full-Stack & Mobile Engineer based in Porto Alegre, Brazil.",
+  EMAIL: "donaldsilveira@gmail.com",
+  CV_LINK: "https://read.cv/donnes",
+  CITY_LINK:
+    "https://www.google.com/maps/@-30.0785166,-51.2481911,3a,80.4y,356.92h,74.03t/data=!3m7!1e1!3m5!1sAF1QipPWf24TQoLGOy0aB54NHGg60BpeJoBb1kEvLPaU!2e10!3e11!7i8192!8i4096?coh=205410&entry=ttu",
+};
